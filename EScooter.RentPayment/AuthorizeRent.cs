fc1ba97abc119183c67e560666ca9a2639f66a4b
Francesco Dente <dente.francesco1996@gmail.com>
2021-11-09 14:25:14 +0100

    Add function code

---
+++ EScooter.RentPayment/AuthorizeRent.cs
@@ -0,0 +1,62 @@
+using Azure.Messaging.ServiceBus;
+using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
+using EasyDesk.CleanArchitecture.Application.Json;
+using EasyDesk.CleanArchitecture.Infrastructure.Events.ServiceBus;
+using EasyDesk.CleanArchitecture.Infrastructure.Json;
+using EasyDesk.CleanArchitecture.Infrastructure.Time;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+
+namespace EScooter.RentPayment
+{
+    public static class AuthorizeRent
+    {
+        public record RentRequested(Guid RentId);
+
+        public record RentPaymentAuthorized(Guid RentId) : ExternalEvent;
+
+        [Function("AuthorizeRent")]
+        public static void Run(
+            [ServiceBusTrigger("%TopicName%", "%SubscriptionName%", Connection = "ServiceBusConnectionString")] string messageContent,
+            FunctionContext context)
+        {
+            var logger = context.GetLogger("AuthorizeRent");
+            var serializer = CreateSerializer();
+            var publisher = CreatePublisher(serializer);
+
+            var rentId = serializer.Deserialize<RentRequested>(messageContent).RentId;
+            publisher.Publish(new RentPaymentAuthorized(rentId));
+
+            logger.LogInformation($"Handled event of type {typeof(RentRequested).Name} for rent '{rentId}'");
+        }
+
+        private static IExternalEventPublisher CreatePublisher(IJsonSerializer jsonSerializer)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
+            var topicName = Environment.GetEnvironmentVariable("TopicName");
+            var descriptor = AzureServiceBusSenderDescriptor.Topic(topicName);
+            var client = new ServiceBusClient(connectionString);
+            var serviceBusPublisher = new AzureServiceBusPublisher(client, descriptor);
+
+            return new ExternalEventPublisher(
+                serviceBusPublisher,
+                new MachineDateTime(),
+                jsonSerializer);
+        }
+
+        private static IJsonSerializer CreateSerializer()
+        {
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                }
+            };
+            return new NewtonsoftJsonSerializer(serializerSettings);
+        }
+    }
+}
